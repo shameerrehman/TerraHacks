5d08314165c5fd720a5d3578d77d11752f3107d6
Shameer Rehman <shameerrehman@shameers-air.wifi.int.ryerson.ca>
2024-08-03 12:15:58 -0400

    adding navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* header */}
+      <Navbar/>
+      {/* cloth card */}
+      {/* button footer */}
+
+      {/* chats screen */}
+      {/* chat person */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
+import './Navbar.css'
+import logo from './logo.png';  // Import the logo image
+import { IconButton } from '@mui/material';
+
+
+function Navbar() {
+  return (
+    <div className='header'>
+        <IconButton>
+            <PersonIcon fontSize='large' />
+        </IconButton>
+        
+        <img className='logo' 
+        src= {logo}
+        alt='style swap'
+        />
+        <IconButton>
+            <ForumIcon fontSize='large' />
+        </IconButton>
+    </div>
+  )
+}
+
+export default Navbar
